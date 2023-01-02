e69510f4e20fae0930c49f9305a16536030fdfa8
Evandro Lovato <evandroslk@gmail.com>
2023-01-02 11:00:06 -0300

    Teste

--- backend/src/main/java/com/devsuperior/dscatalog/DscatalogApplication.java
+++ backend/src/main/java/com/devsuperior/dscatalog/DscatalogApplication.java
@@ -6,6 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DscatalogApplication {
 
+	// Teste
+
 	public static void main(String[] args) {
 		SpringApplication.run(DscatalogApplication.class, args);
 	}
